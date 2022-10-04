3a0533b9f54f9f57aff174da11e0c567a112fcba
andrea <andrealiureta@gmail.com>
2022-10-04 20:23:46 +0400

    Fix background for splash screen

--- app/src/main/java/id/worx/device/client/screen/welcome/SplashScreen.kt
+++ app/src/main/java/id/worx/device/client/screen/welcome/SplashScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,6 +26,7 @@ fun SplashScreen() {
             Image(
                 modifier = Modifier.fillMaxSize()
                     .background(color = PrimaryMain),
+                contentScale = ContentScale.Crop,
                 painter = painterResource(R.drawable.grid_bg),
                 contentDescription = "Background")
             Image(
