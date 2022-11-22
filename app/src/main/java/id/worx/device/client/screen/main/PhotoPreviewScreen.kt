dbacd2714d84c3b8fd16b30d6a60349d011be4d5
DESKTOP-03UCHHM\pohsn <rafflypohan17@gmail.com>
2022-11-22 15:55:06 +0800

    Update icon retake & confirm in camera capture preview

--- app/src/main/java/id/worx/device/client/screen/main/PhotoPreviewScreen.kt
+++ app/src/main/java/id/worx/device/client/screen/main/PhotoPreviewScreen.kt
@@ -3,8 +3,14 @@ package id.worx.device.client.screen.main
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,6 +31,14 @@ fun PhotoPreviewScreen(
     detailViewModel: DetailFormViewModel,
     addPhotoToGallery: (String) -> Unit
 ) {
+    val detailUiState = detailViewModel.uiState.collectAsState()
+
+    val path = viewModel.photoPath.value!!
+    val index = viewModel.indexForm.value!!
+    val id = detailUiState.value.detailForm!!.fields[index].id
+    val value = detailUiState.value.values[id] as ImageValue?
+    val filePath = value?.filePath ?: arrayListOf()
+
     Box(modifier = Modifier.fillMaxSize()) {
         AsyncImage(
             model = viewModel.photoPath.value?.let { File(it) },
@@ -41,42 +55,133 @@ fun PhotoPreviewScreen(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Text(
-                text = "x",
-                fontSize = 36.sp,
-                color = Color.White,
-                modifier = Modifier
-                    .padding(horizontal = 36.dp, vertical = 20.dp)
-                    .clickable { viewModel.rejectPhoto() }
-            )
-            Text(
-                text = "\u2713",
-                fontSize = 36.sp,
-                color = Color.White,
-                modifier = Modifier
-                    .padding(horizontal = 36.dp, vertical = 20.dp)
-                    .clickable {
-                        val path = viewModel.photoPath.value!!
-                        val index = viewModel.indexForm.value!!
-                        val id = detailViewModel.uiState.value.detailForm!!.fields[index].id
-                        val value = detailViewModel.uiState.value.values[id] as ImageValue?
-                        val filePath = value?.filePath ?: arrayListOf()
-                        filePath.add(path)
-                        detailViewModel.getPresignedUrl(filePath, index, 2)
-                        addPhotoToGallery(path)
-                        viewModel.navigateToDetail()
-                    }
-            )
+            TextButton(onClick = {
+                viewModel.rejectPhoto()
+            }, modifier = Modifier.padding(8.dp)) {
+                Icon(
+                    imageVector = Icons.Default.Refresh,
+                    contentDescription = null,
+                    tint = Color.White,
+                    modifier = Modifier.size(16.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = "Retake".uppercase(), fontSize = 16.sp,
+                    color = Color.White,
+
+                    )
+            }
+            TextButton(onClick = {
+                filePath.add(path)
+                detailViewModel.getPresignedUrl(filePath, index, 2)
+                addPhotoToGallery(path)
+                viewModel.navigateToDetail()
+            }, modifier = Modifier.padding(8.dp)) {
+                Icon(
+                    imageVector = Icons.Default.Done,
+                    contentDescription = null,
+                    tint = Color.White,
+                    modifier = Modifier.size(16.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = "Done".uppercase(), fontSize = 16.sp,
+                    color = Color.White,
+                )
+            }
         }
     }
 }
 
-@Preview
-@Composable
-private fun PreviewPhotoScreen() {
-    val viewModel: CameraViewModel = hiltViewModel()
-    val detailViewModel: DetailFormViewModel = hiltViewModel()
-    WorxTheme() {
-        PhotoPreviewScreen(viewModel = viewModel, detailViewModel) {}
-    }
-}
+//@Preview
+//@Composable
+//private fun PreviewPhotoScreen() {
+//    val viewModel: CameraViewModel = hiltViewModel()
+//    val detailViewModel: DetailFormViewModel = hiltViewModel()
+//    WorxTheme() {
+//        PhotoPreviewScreen(viewModel = viewModel, detailViewModel) {}
+//    }
+//}
+
+//@Composable
+//fun TestScreen(
+//    viewModel: CameraViewModel,
+//    detailViewModel: DetailFormViewModel,
+//    addPhotoToGallery: (String) -> Unit
+//) {
+//    val model = viewModel.photoPath.value?.let { File(it) }
+//    val onRejectPhotoClicked = viewModel.rejectPhoto()
+//    val path = viewModel.photoPath.value!!
+//    val index = viewModel.indexForm.value!!
+//    val state = detailViewModel.uiState.collectAsState()
+//    val id = state.value.detailForm!!.fields[index].id
+//    val value = state.value.values[id] as ImageValue?
+//    val filePath = value?.filePath ?: arrayListOf()
+//
+//    // taro di onclick
+//
+//    TestChildScreen(model, { onRejectPhotoClicked }) {
+//        filePath.add(path)
+//        detailViewModel.getPresignedUrl(filePath, index, 2)
+//        addPhotoToGallery(path)
+//        viewModel.navigateToDetail()
+//    }
+//}
+//
+//@Composable
+//fun TestChildScreen(model: File?, function: () -> Unit, function1: () -> Unit) {
+//    Box(modifier = Modifier.fillMaxSize()) {
+//        AsyncImage(
+//            model = model,
+//            contentDescription = "Image",
+//            modifier = Modifier
+//                .fillMaxSize()
+//        )
+//        Row(
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .height(110.dp)
+//                .background(Color.Black.copy(0.54f))
+//                .align(Alignment.BottomCenter),
+//            verticalAlignment = Alignment.CenterVertically,
+//            horizontalArrangement = Arrangement.SpaceBetween
+//        ) {
+//            TextButton(onClick = { /*TODO*/ }, modifier = Modifier.padding(8.dp)) {
+//                Icon(
+//                    imageVector = Icons.Default.Refresh,
+//                    contentDescription = null,
+//                    tint = Color.White,
+//                    modifier = Modifier.size(16.dp)
+//                )
+//                Spacer(modifier = Modifier.width(8.dp))
+//                Text(
+//                    text = "Retake".uppercase(), fontSize = 16.sp,
+//                    color = Color.White,
+//
+//                    )
+//            }
+//            TextButton(onClick = { /*TODO*/ }, modifier = Modifier.padding(8.dp)) {
+//                Icon(
+//                    imageVector = Icons.Default.Done,
+//                    contentDescription = null,
+//                    tint = Color.White,
+//                    modifier = Modifier.size(16.dp)
+//                )
+//                Spacer(modifier = Modifier.width(8.dp))
+//                Text(
+//                    text = "Done".uppercase(), fontSize = 16.sp,
+//                    color = Color.White,
+//                )
+//            }
+//        }
+//    }
+//}
+//
+//@Preview
+//@Composable
+//fun TesPreview() {
+//    TestChildScreen(model = null, function = {}) {
+//
+//    }
+//
+//}
