e7476084ad42a75d3f97f03161fdf4773e06c75c
DESKTOP-03UCHHM\pohsn <rafflypohan17@gmail.com>
2022-12-06 12:36:12 +0800

    Revert "Add reset button in radio button section"
    This reverts commit 7d6df96505d935a1301b10281f297afbd894621b.

--- app/src/main/java/id/worx/device/client/screen/components/WorxRadioButton.kt
+++ app/src/main/java/id/worx/device/client/screen/components/WorxRadioButton.kt
@@ -5,8 +5,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
@@ -47,8 +45,7 @@ fun WorxRadiobutton(
     } else {
         remember { mutableStateOf<Int?>(null) }
     }
-    val warningInfo =
-        if (form.required == true && onCheck.value == null) "$title is required" else ""
+    val warningInfo = if (form.required == true && onCheck.value == null) "$title is required" else ""
 
     Column(modifier = Modifier.fillMaxWidth()) {
         Text(
@@ -97,32 +94,6 @@ fun WorxRadiobutton(
                         style = Typography.body1.copy(MaterialTheme.colors.onSecondary)
                     )
                 }
-            }
-        }
-        if (!arrayListOf(
-                EventStatus.Done,
-                EventStatus.Submitted
-            ).contains(formStatus)
-        ) {
-            TextButton(
-                onClick = {
-                    onCheck.value = null
-                },
-                modifier = Modifier
-                    .align(Alignment.End)
-                    .padding(top = 8.dp)
-                    .padding(horizontal = 16.dp),
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Refresh,
-                    contentDescription = "Reset Icon",
-                    tint = MaterialTheme.colors.onBackground
-                )
-                Text(
-                    text = "Reset",
-                    style = Typography.body2.copy(MaterialTheme.colors.onBackground),
-                    modifier = Modifier.padding(start = 8.dp)
-                )
             }
         }
         if (validation && warningInfo.isNotBlank()) {
