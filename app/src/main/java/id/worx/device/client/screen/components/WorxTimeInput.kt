d252cdc2df0415dc5281c1c7378aef225159ff53
Andrea <andrealiureta@gmail.com>
2022-12-16 23:10:34 +0400

    add custom number picker

--- app/src/main/java/id/worx/device/client/screen/components/WorxTimeInput.kt
+++ app/src/main/java/id/worx/device/client/screen/components/WorxTimeInput.kt
@@ -2,8 +2,8 @@ package id.worx.device.client.screen.components
 
 import android.app.AlertDialog
 import android.app.Dialog
-import android.app.TimePickerDialog
 import android.content.Context
+import android.widget.Button
 import android.widget.TextView
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -15,9 +15,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.core.content.res.ResourcesCompat
 import id.worx.device.client.R
 import id.worx.device.client.data.database.Session
 import id.worx.device.client.model.TimeField
@@ -43,7 +45,7 @@ fun WorxTimeInput(indexForm: Int, viewModel: DetailFormViewModel, session: Sessi
     val context = LocalContext.current
     val warningInfo = if (form.required == true && value.value == null) "${form.label} is required" else ""
 
-    val timePickerCallback = TimePickerDialog.OnTimeSetListener { timePicker, hr, min ->
+    val timePickerCallback = { hr: Int, min: Int ->
         value.value = id.worx.device.client.model.LocalTime(hour = hr, minute = min)
         viewModel.setComponentData(indexForm, TimeValue(value = value.value))
         showTimePicker = false
@@ -56,8 +58,10 @@ fun WorxTimeInput(indexForm: Int, viewModel: DetailFormViewModel, session: Sessi
     }
     val mTimeSliderDialog = WorxTimeSliderDialog(
         context,
-        style,
-        value.value?.hour ?: 0, value.value?.minute ?: 0
+        colorTheme = if (theme == SettingTheme.Dark) PrimaryMain else MaterialTheme.colors.primary,
+        hr =value.value?.hour ?: 0,
+        min = value.value?.minute ?: 0,
+        onTimePickerListener = timePickerCallback
     )
 
     Column(modifier = Modifier
@@ -82,7 +86,8 @@ fun WorxTimeInput(indexForm: Int, viewModel: DetailFormViewModel, session: Sessi
                 .fillMaxWidth()
         ) {
             TextField(
-                modifier = Modifier.padding(end = 12.dp)
+                modifier = Modifier
+                    .padding(end = 12.dp)
                     .weight(1f),
                 enabled = false,
                 colors = TextFieldDefaults.textFieldColors(
@@ -143,13 +148,43 @@ fun WorxTimeInput(indexForm: Int, viewModel: DetailFormViewModel, session: Sessi
     }
 }
 
-class WorxTimeSliderDialog(context: Context, style:Int, hr: Int, min: Int) : AlertDialog(context) {
+class WorxTimeSliderDialog(
+    context: Context,
+    val colorTheme:Color,
+    val hr: Int,
+    val min: Int,
+    val onTimePickerListener: (Int, Int)-> Unit) : AlertDialog(context) {
+
     fun showDialog(){
         val dialog = Dialog(context)
+        dialog.setTitle("Select Time")
+
+        val title = dialog.window?.decorView?.findViewById<TextView>(android.R.id.title)
+        title?.textSize = 16f
+        title?.typeface = ResourcesCompat.getFont(context, R.font.dmmono)
+
         dialog.setContentView(R.layout.time_picker_dialog)
-        val cancel = dialog.findViewById<TextView>(
-            R.id.btn_cancel
-        ).apply { setOnClickListener { dialog.dismiss() } }
+
+        val hrPicker = dialog.findViewById<com.shawnlin.numberpicker.NumberPicker>(R.id.hour_picker)
+        hrPicker.setFormatter(R.string.number_picker_formatter)
+        hrPicker.value = hr
+        hrPicker.selectedTextColor = colorTheme.toArgb()
+
+        val minPicker = dialog.findViewById<com.shawnlin.numberpicker.NumberPicker>(R.id.min_picker)
+        minPicker.setFormatter(R.string.number_picker_formatter)
+        minPicker.value = min
+        minPicker.selectedTextColor = colorTheme.toArgb()
+
+        val cancelButton = dialog.findViewById<TextView>(R.id.btn_cancel)
+        cancelButton.setTextColor(colorTheme.toArgb())
+        cancelButton.setOnClickListener { dialog.dismiss() }
+
+        val  okButton = dialog.findViewById<Button>(R.id.btn_ok)
+        okButton.setBackgroundColor(colorTheme.toArgb())
+        okButton.setOnClickListener {
+            onTimePickerListener(hrPicker.value, minPicker.value)
+            dialog.dismiss()
+        }
         dialog.show()
     }
 }
