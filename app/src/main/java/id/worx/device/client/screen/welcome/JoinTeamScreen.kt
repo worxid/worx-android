907699b430239deffa3d29ca6f3b9d0febeaed69
victor <victor@tokopedia.com>
2023-08-30 18:05:15 +0700

    optimize import

--- app/src/main/java/id/worx/device/client/screen/welcome/JoinTeamScreen.kt
+++ app/src/main/java/id/worx/device/client/screen/welcome/JoinTeamScreen.kt
@@ -13,7 +13,11 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -27,7 +31,6 @@ import id.worx.device.client.R
 import id.worx.device.client.data.database.Session
 import id.worx.device.client.screen.components.RedFullWidthButton
 import id.worx.device.client.screen.components.WorxTextField
-import id.worx.device.client.screen.components.WorxTopAppBar
 import id.worx.device.client.theme.Typography
 import id.worx.device.client.theme.WorxTheme
 
